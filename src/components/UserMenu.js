8d40dd50c552fb0c8df79973d9698b2643d212eb
Vitalii Shubnik <vitalii.shubnik@gmail.com>
2022-06-15 17:24:44 +0300

    added logout fake function

--- src/components/UserMenu.js
+++ src/components/UserMenu.js
@@ -10,7 +10,7 @@ const UserMenu = () => {
     <div className='user-menu'>
       <Hamburger color='#313131' size={9} toggled={isOpen} toggle={setOpen} />
       <img className='avatar' src={avatar} height={32} />
-      <button className={isOpen ? 'logout-btn':'hidden'} >Logout</button>
+      <button className={isOpen ? 'logout-btn':'hidden'} onClick={logout} >Logout</button>
     </div>
   )
 }
