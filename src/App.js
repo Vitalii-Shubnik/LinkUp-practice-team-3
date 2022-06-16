de1e4162b431c0526ca45d3f64ad6e89853ab0f0
Василь Лютан <vasyl.liutan.pz.2019@lpnu.ua>
2022-06-16 20:42:18 +0300

    all_product_v1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { React } from 'react'
-import SignUp from './pages/SignUp';
-import {Link} from 'react-router-dom'
+import { React } from "react";
+// import SignUp from "./pages/SignUp";
+import { Link } from "react-router-dom";
+import AllProduct from "./components/AllProduct";
+import products from "./components/data.js";
+
+// import { Provider } from "speedux";
 function App() {
   return (
-    <div>
-      <Link
-        to='sign-up'
-      >
-        sign-up
-      </Link>
+    <div className="conteiner">
+      <Link to="sign-up">sign-up</Link>
+      <div className="shop">
+        <AllProduct items={products} />
+      </div>
     </div>
   );
 }
