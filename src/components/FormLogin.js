4fa7aeee98b9f8a9d2c0538ba831dcafee6c0d74
Василь Лютан <vasyl.liutan.pz.2019@lpnu.ua>
2022-06-22 14:23:25 +0300

    add_yup_validation

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -1,71 +1,73 @@
 import React from "react";
 import "../styles/Login.css";
 import { Formik } from "formik";
+import * as Yup from "yup";
 
-const Basic = () => (
-  <div>
-    <Formik
-      initialValues={{ email: "", password: "" }}
-      validate={(values) => {
-        const errors = {};
-        if (!values.email) {
-          errors.email = "Required";
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = "Invalid email address";
-        }
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-        /* and other goodies */
-      }) => (
-        <form className="form2" onSubmit={handleSubmit}>
-          <input
-            className="input1"
-            type="email"
-            name="email"
-            placeholder="Email"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.email}
-          />
-          <div className="errors">
-            {errors.email && touched.email && errors.email}
-          </div>
-          <input
-            className="input1"
-            type="password"
-            name="password"
-            placeholder="Password"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.password}
-          />
-          <div className="errors">
-            {errors.password && touched.password && errors.password}
-          </div>
-          <button className="login-sbm" type="submit" disabled={isSubmitting}>
-            Login
-          </button>
-        </form>
-      )}
-    </Formik>
-  </div>
-);
+const Basic = () => {
+  const SignupSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid email").required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password is too short!")
+      .max(50, "Password is too long!")
+      .required("Password is required"),
+  });
+  return (
+    <>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <form className="form2" onSubmit={handleSubmit}>
+            <input
+              className="input1"
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+            <input
+              className="input1"
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+            />
+            <div className="errors">
+              {errors.email && touched.email && errors.email}
+            </div>
+            <div className="errors">
+              {errors.password && touched.password && errors.password}
+            </div>
+            <button className="login-sbm" type="submit" disabled={isSubmitting}>
+              Login
+            </button>
+          </form>
+        )}
+      </Formik>
+    </>
+  );
+};
 
 export default Basic;
