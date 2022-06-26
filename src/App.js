651923c6ca0dd530d8ae4a1c6e7d77b06c4517ab
Василь Лютан <vasyl.liutan.pz.2019@lpnu.ua>
2022-06-26 15:41:09 +0300

    add_checkout

--- src/App.js
+++ src/App.js
@@ -7,13 +7,23 @@ import products from "./components/data.js";
 import { Link } from "react-router-dom";
 import NavBar from "./components/Navbar";
 import Cart from "./components/Cart";
+import props_cart from "./components/data2.js";
 
 function App() {
   const [visionCard, setVisionCard] = useState(false);
+  const [visionCheckout, setVisionCheckout] = useState(false);
+  console.log(visionCard);
+  // className={`none_opacity ${visionCard && "block_opacity"}`}
   return (
     <div>
       <NavBar visionCard={visionCard} setVisionCard={setVisionCard} />
-      <Cart visionCard={visionCard} setVisionCard={setVisionCard} />
+      <Cart
+        items={props_cart}
+        visionCard={visionCard}
+        setVisionCard={setVisionCard}
+        visionCheckout={visionCheckout}
+        setVisionCheckout={setVisionCheckout}
+      />
     </div>
   );
 }
