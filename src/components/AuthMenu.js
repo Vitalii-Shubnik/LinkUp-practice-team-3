7a421df08eb1090e6da3c3da1964eb6e9d462c68
Vitalii Shubnik <vitalii.shubnik@gmail.com>
2022-06-27 17:17:11 +0300

    added navigation to log in page

--- src/components/AuthMenu.js
+++ src/components/AuthMenu.js
@@ -4,14 +4,16 @@ import { useNavigate } from "react-router-dom";
 const AuthMenu = () => {
   const navigate = useNavigate();
 
-  const handleClick = () => {
+  const handleClickSignUp = () => {
     navigate('/sign-up');
   }
-
+  const handleClickLogIn = () => {
+    navigate('/sign-in');
+  }
   return (
     <div className='auth-menu'>
-      <button className='auth-btn log-in-btn' onClick={handleClick}>Log in</button>
-      <button className='auth-btn sign-up-btn' onClick={handleClick}>Sign up</button>
+      <button className='auth-btn log-in-btn' onClick={handleClickLogIn}>Log in</button>
+      <button className='auth-btn sign-up-btn' onClick={handleClickSignUp}>Sign up</button>
     </div>
   )
 }
